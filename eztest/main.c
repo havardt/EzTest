ddf9f9791b1a494b87ebfd0d0a6ef071e9a99b03
havardt <35465108+havardt@users.noreply.github.com>
2019-07-20 20:47:02 +0200

    Base options functionality

---
+++ eztest/main.c
@@ -0,0 +1,143 @@
+/**
+ 
+* @author  havardt <https://github.com/havardt>
+*
+* @license MIT     <https://opensource.org/licenses/MIT>
+*
+*/
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <assert.h>
+#include <stdio.h>
+
+
+/* Structs */
+
+/** Represents the application options. */
+struct options
+{
+    /** When set to @code true @endcode only default color is used when printing. */
+    bool no_color;
+};
+
+
+/* Macros */
+
+#define __PROGRAM_NAME__ "EzTest"
+#define __PROGRAM_VERSION__ "0.0.1"
+#define __PROGRAM_AUTHOR__ "havardt"
+#define __PROGRAM_WEBSITE__ "https://github.com/havardt/EzTest"
+
+/** Represents the standard ok/ success result value for non-pointer return types. */
+#define RESULT_OK 0
+
+/** Represents the standard error/ fail result value for non-pointer return types. */
+#define RESULT_ERR -1
+
+#define DEFAULT_OPTIONS { .no_color = false }
+
+
+/* Prototypes */
+
+void      print_usage ( FILE            *                     );
+const int parse_opt   ( struct  options *, const int          );
+const int handle_opts ( struct  options *, const int, char ** );
+
+
+/* Globals */
+
+const struct option long_opts[] = {
+    {"help"     , 0, NULL, 'h'},
+    {"version"  , 0, NULL, 'v'},
+    {"no-color" , 0, NULL, 'c'}
+};
+
+
+int main(int argc, char **argv)
+{
+    struct options opts = DEFAULT_OPTIONS;
+
+    if(handle_opts(&opts, argc, argv) != RESULT_OK)
+    {
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+/** 
+ * Prints program usage.
+ * 
+ * @param fd A file descriptor representing the output location.
+ */
+void print_usage(FILE *fd)
+{
+    assert(fd != NULL);
+
+    fprintf(fd, "\nUsage: %s [OPTIONS]\n\n"
+                "Options:\n"
+                " -v --version  Print version number.\n"
+                " -h --help     Print help information.\n"
+                " -c --no-color Only use default color when printing to screen.\n\n",
+                __PROGRAM_NAME__);
+}
+
+/**
+ * Parse application options.
+ *  
+ * @param opts Pointer to current options.
+ * @param opt  The option to parse.
+ * @return: On success @code RESULT_OK @endcode; otherwise @code RESULT_ERR @endcode .
+ */
+const int parse_opt(struct options *opts, const int opt)
+{
+    switch(opt)
+    {
+        case 'v':
+            printf("%s version %s\n", __PROGRAM_NAME__, __PROGRAM_VERSION__);
+            exit(EXIT_SUCCESS);
+            break;
+        
+        case 'h':
+            print_usage(stdout); 
+            exit(EXIT_SUCCESS);
+            break;
+
+        case 'c':
+            opts->no_color = true;
+            break;
+
+        default:
+            return RESULT_ERR;
+    }
+    return RESULT_OK;
+}
+
+/**
+ * Processes application options by parsing given options and setting the 
+ * appropriate options.
+ * 
+ * @param opts Pointer to application options struct.
+ * @param argc Argument count.
+ * @param argv Arguments as strings.
+ * @return @code RESULT_OK @endcode on success and @code RESULT_ERR @endocode on failure. 
+ *         Failure may occur if the user has given an unknown option. 
+ */
+const int handle_opts(struct options *opts, const int argc, char **argv)
+{
+    int opt, opt_index;
+    while((opt = getopt_long(argc, argv, "vhc", long_opts, &opt_index)) != -1)
+    {
+        if(parse_opt(opts, opt) != RESULT_OK)
+        {
+            print_usage(stderr); 
+            return RESULT_ERR;
+        }
+    }
+    return RESULT_OK;
+}
+
