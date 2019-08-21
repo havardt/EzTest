87cff938211774902e142410781b0bad038f81dd
havardt <35465108+havardt@users.noreply.github.com>
2019-08-21 23:33:39 +0200

    Update v1.0.0 -> v1.1.0

--- eztest/core/about.h
+++ eztest/core/about.h
@@ -10,7 +10,7 @@
 #define ABOUT_H
 
 #define __PROGRAM_NAME__ "EzTest"
-#define __PROGRAM_VERSION__ "1.0.0"
+#define __PROGRAM_VERSION__ "1.1.0"
 #define __PROGRAM_AUTHOR__ "havardt"
 #define __PROGRAM_WEBSITE__ "https://github.com/havardt/EzTest"
 
